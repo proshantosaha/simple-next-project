542ee6a6b859f416d31d67cd14046be0244c5868
proshantosaha <proshantoll71@gmail.com>
2023-07-21 21:17:10 +0600

    create contact from and add postcontact api

---
+++ pages/contact.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import styles from '@/styles/Contact.module.css'
+
+
+const Contact = () => {
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [phone,setPhone] = useState('')
+  const [desc,setDesc] = useState('')
+
+
+
+
+  const handleChange = (e) =>{
+
+    if(e.target.name==="phone"){
+      setPhone(e.target.value)
+    }
+    else if(e.target.name==="email"){
+      setEmail(e.target.value)
+    }
+    
+    else if(e.target.name==="name"){
+      setName(e.target.value)
+    }
+    
+    else if(e.target.name==="desc"){
+      setDesc(e.target.value )
+    }
+    
+    
+      
+
+    console.log();
+  }
+
+
+  const handleSubmit = (e) =>{
+      e.preventDefault()
+      console.log(phone,name,email,desc);
+
+
+      async function postJSON(data) {
+        try {
+          const response = await fetch("http://localhost:3000/api/postContact", {
+            method: "POST", // or 'PUT'
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+      
+          const result = await response.json();
+          console.log("Success:", result);
+          alert("thanks for contacting us")
+          setName('')
+          setEmail('')
+          setPhone('')
+          setDesc('')
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
+      
+      const data = {phone,email,name,desc};
+      postJSON(data);
+  }
+
+  return (
+
+
+    <div className={styles.container}>
+      <div>contact us</div>
+      <form onSubmit={handleSubmit   }>
+        <div className={styles.mb3}>
+          <label htmlFor="name" className={styles.formlabel}>Name</label>
+          <input type="text" value={name} onChange={handleChange} className={styles.formcontrol} id="name" name="name" />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="email" className={styles.formlabel}>Email address</label>
+          <input type="email" value={email} onChange={handleChange} className={styles.formcontrol} id="email" name="email" />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="phone" className={styles.formlabel}>Password</label>
+          <input type="phone" value={phone} onChange={handleChange} className={styles.formcontrol} id="phone" name="phone" />
+        </div>
+
+
+        <div className={styles.mb3}>
+          <label htmlFor="desc">Comments</label>
+          <textarea value={desc} className={styles.formcontrol} name='desc' onChange={handleChange} placeholder="comment here" id="desc"></textarea>
+        </div>
+
+
+        <button type="submit" className={styles.btnprimary}>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Contact
